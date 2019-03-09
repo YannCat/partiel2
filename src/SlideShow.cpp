768084c73443bc19dac19389c2b4a13896f2e2fc
Yann CATALA <yann@plusdebornes.fr>
2019-03-09 10:17:26 +0100

    Fin question 2a

---
+++ src/SlideShow.cpp
@@ -0,0 +1,111 @@
+// Copyright (C) 2017 Ange Abou - Polytech'Nice
+//
+// Ce programme est un logiciel libre; Il peut être redistribué et/ou
+// modifié sous les termes de la "GNU General Public License" (Licence
+// Générale Publique GNU) comme publiés par la Free Software Foundation;
+// soit la version 2, soit (au choix) toute version ultérieure.
+//
+// Ce programme a une vocation éducative, et est distribué sans aucune
+// garantie de fonctionnement; sans la garantie implicite de pouvoir 
+// le commercialiser ou même de répondre à un besoin particulier.
+// Se référer à la "GNU General Public License" pour plus de détails.
+//
+// Date de création: Mai 2017
+
+#include "SlideShow.hpp"
+#include <iostream>
+// Pour générer des valeurs entières aléatoires uniformément réparties
+#include <random>
+
+using namespace std;
+
+
+#define CHECK_IF_INITIALISED() \
+    if (!_isInitialised) { \
+        cerr << "Attention: appeler d'abord init sur objet SlideShow." << endl;\
+        return 0;\
+    }
+
+/* */
+SlideShow::SlideShow(const char* iFilePath) 
+    : _filepath(iFilePath)
+    , _filestream(iFilePath)
+    , _nbFilelines(0) 
+    , _isInitialised(false) {
+    
+}
+
+/* */
+SlideShow::SlideShow(const std::string iFilePath) 
+    : SlideShow(iFilePath.c_str()) {
+    // Ce constructeur avec paramètre 'const string' réutilise le constructeur
+    // précédent avec paramètre 'const char*'
+}
+ 
+/* */
+bool SlideShow::init() {
+    
+    ifstream _filestream(_filepath, ifstream::in);
+    if (_filestream.good()) {
+        this->_nbFilelines = 0;
+        string ligne;
+        while(getline(_filestream, ligne)){
+            this->_nbFilelines++;
+        }
+        
+        this->_isInitialised=true;
+    } else {
+        
+        cerr << "Erreur: impossible d'ouvrir le fichier." << endl;
+        this->_isInitialised=false;
+        return false;
+    }
+    
+    return true;
+}
+
+const std::uint64_t SlideShow::getNumberOfSlides() {
+    CHECK_IF_INITIALISED();
+    
+    return _nbFilelines;
+}
+
+/*
+ http://fr.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
+ Pour produire des valeurs entières aléatoires uniformément réparties
+ */
+const std::uint64_t SlideShow::getRandomNumber() {
+    CHECK_IF_INITIALISED();
+    
+    static random_device rd;
+    static mt19937 gen; // générateur
+    static uniform_int_distribution<> dis(1, 6);
+    /*
+    * TODO: Expliquez dans les commentaires 
+    * 
+    * - Pourquoi on utilise le mot clé 'static' ici ? (Contrairement au code en ligne)
+    *
+    * - Modifiez le code pour choisir les nombres aléatoires jusqu'au nombre de slides
+    */
+    
+    return dis(gen);
+}
+
+const std::uint8_t SlideShow::displayRandomSlide(std::ostream* out) {
+    std::uint8_t rc = 0; // code de retour si OK
+    if (!_isInitialised) {
+        cerr << "Attention: appeler d'abord init sur objet SlideShow." << endl;
+        rc = 1; // code d'erreur si non initialisé
+    }
+    
+    /*
+    * TODO: complétez le code ...
+    * 
+    * Le but est d'afficher sur la sortie standard, de façon aléatoire,
+    * le contenu d'une ligne du fichier
+    * correspondant au nombre aléatoire obtenu
+    */
+    *out << "completez le code ... et enlevez ceci!" << endl;
+    
+    return rc;
+}
